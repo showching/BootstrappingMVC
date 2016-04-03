8f258c30def83ccb7cb9f1b0680fbcbc067a9732
Showching <tong.xiaocheng@live.com>
2016-04-03 22:46:17 +0800

    day-11

---
+++ BootstrappingMVC/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using BootstrappingMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrappingMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            GenFu.GenFu.Configure<Person>()
+                .Fill(p => p.LikesMusic).WithRandom(new List<bool>() { true, true, true, false, false })
+                .Fill(p => p.Skills, () => new List<string>() { "Math", "Science", "History" });
+            var people = GenFu.A.ListOf<Person>(20);
+            return View(people);
+        }
+    }
+}
