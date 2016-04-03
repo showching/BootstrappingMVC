9d5ad7b507ece3e593caea68dad3210635c845b4
Showching <tong.xiaocheng@live.com>
2016-04-03 23:35:35 +0800

    day-12

--- BootstrappingMVC/Controllers/PersonController.cs
+++ BootstrappingMVC/Controllers/PersonController.cs
@@ -9,14 +9,32 @@ namespace BootstrappingMVC.Controllers
 {
     public class PersonController : Controller
     {
-        // GET: Person
-        public ActionResult Index()
+        private static List<Person> _people;
+
+        static PersonController()
         {
             GenFu.GenFu.Configure<Person>()
                 .Fill(p => p.LikesMusic).WithRandom(new List<bool>() { true, true, true, false, false })
                 .Fill(p => p.Skills, () => new List<string>() { "Math", "Science", "History" });
-            var people = GenFu.A.ListOf<Person>(20);
-            return View(people);
+            _people = GenFu.A.ListOf<Person>(20);
+        }
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(_people);
+        }
+
+        public ActionResult SearchPeople(string searchText)
+        {
+            var term = searchText.ToLower();
+            var result = _people
+                .Where(p =>
+                    p.FirstName.ToLower().Contains(term) ||
+                    p.LastName.ToLower().Contains(term)
+                );
+
+            return PartialView("_SearchPeople", result);
         }
     }
 }
